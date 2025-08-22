255923d4ff185f14ef1b9100369ac4c0bcff3c22
Jesusxd96 <33816389+Jesusxd96@users.noreply.github.com>
2025-08-21 21:39:03 -0600

    Scene Cleanup
    - Removed Third Person Cam, Orientation and PlayeObj.
    - Restarted the "PlayerController" script.

--- Taami/Assets/_Scripts/PlayerController.cs
+++ Taami/Assets/_Scripts/PlayerController.cs
@@ -7,13 +7,14 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private Vector2 inputDirection;
     private float forceMagnitude;//La velocidad del personaje pues.
-
+    
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
+    /*
     private void FixedUpdate()
     {
         rb.AddForce(new Vector3(0, forceMagnitude, 0), ForceMode.VelocityChange);
-    }
+    }*/
 }
