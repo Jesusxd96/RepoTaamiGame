7e13cd38787c6f1b7e869ddd087833dea8de1f2b
Jesusxd96 <33816389+Jesusxd96@users.noreply.github.com>
2025-08-22 19:07:44 -0600

    Movement Script and Camera
    - Readded the cinemachine camera and configured to follow the Player.
    - Coded a basic movement script that takes into consideration the camera direction when moving the Player.

--- Taami/Assets/_Scripts/PlayerController.cs
+++ Taami/Assets/_Scripts/PlayerController.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rb;
-    private Vector2 inputDirection;
-    private float forceMagnitude;//La velocidad del personaje pues.
+    /*For testing purposes we are using the old input system, once it moves we will be moving from this to the new one*/
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f; //La velocidad a la que se mueve el personaje
     
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
+    public float turnSmoothTime = 0.1f; //La velocidad a la que rota el mono, para que no sea tan brusco.
+    float turnSmoothVelocity;
+
+    private void Update()
+    {   /*We get the inputs here*/
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        
+        if(direction.magnitude >= 0.1f) /*Here is for the rotation.*/
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg+ cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            /*This is to move while taking into consideration the camera.*/
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
     }
     /*
     private void FixedUpdate()
