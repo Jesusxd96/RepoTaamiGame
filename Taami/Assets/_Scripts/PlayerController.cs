77290f1698eed582e4076064d3f8f851e17e415f
Jesusxd96 <33816389+Jesusxd96@users.noreply.github.com>
2025-08-27 20:28:39 -0600

    Basic Jump Added
    - Asigned a Rigidbody to the Player.
    - Basic jump logic implemented..
    - Created a folder to store the Inputs for the New Input System.

--- Taami/Assets/_Scripts/PlayerController.cs
+++ Taami/Assets/_Scripts/PlayerController.cs
@@ -7,11 +7,21 @@ public class PlayerController : MonoBehaviour
     /*For testing purposes we are using the old input system, once it moves we will be moving from this to the new one*/
     public CharacterController controller;
     public Transform cam;
+    public Rigidbody rb;//El rigidbody del player.
 
+    /*Variables de movimiento y del jugador*/
     public float speed = 6f; //La velocidad a la que se mueve el personaje
+    public float jumpForce = 10f;
+    public bool isGrounded;
     
     public float turnSmoothTime = 0.1f; //La velocidad a la que rota el mono, para que no sea tan brusco.
     float turnSmoothVelocity;
+
+    private void Start()
+    {
+        isGrounded = true;
+        rb = this.gameObject.GetComponent<Rigidbody>();//Se obtiene el Rigidbody del objeto/personaje.
+    }
 
     private void Update()
     {   /*We get the inputs here*/
@@ -28,10 +38,17 @@ public class PlayerController : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+        /*Logica de Salto simple*/
+        if ((Input.GetButton("Jump"))&&(isGrounded==true))//Se usa la variable interna en lo que se ve como hacerle para la de Controller.
+        {
+            rb.AddForce(transform.up * jumpForce);
+            //rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.VelocityChange);
+            //isGrounded = false;
+        }
     }
     /*
-    private void FixedUpdate()
+    private void FixedUpdate() //Movimiento usando rigidbody.
     {
-        rb.AddForce(new Vector3(0, forceMagnitude, 0), ForceMode.VelocityChange);
+        rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.VelocityChange);//Ignora gravedad y Drag
     }*/
 }
