3bae357befbe8fe660ff04ae4b2ba6db6f99cb47
Jesusxd96 <33816389+Jesusxd96@users.noreply.github.com>
2025-08-28 17:43:34 -0600

    Movement Rework
    - Made it so that the movement is produced utilizing the new input system.
    - Jump and gravity are currently disabled but will be implemented in the next iteration.

--- Taami/Assets/_Scripts/PlayerController.cs
+++ Taami/Assets/_Scripts/PlayerController.cs
@@ -1,62 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]//Esto se asegura de que no se pueda quitar algun componente del objeto
 public class PlayerController : MonoBehaviour
 {
     /*For testing purposes we are using the old input system, once it moves we will be moving from this to the new one*/
-    public CharacterController controller;
     public Transform cam;
     public Rigidbody rb;//El rigidbody del player.
 
     /*Variables de movimiento y del jugador*/
-    public float speed = 6f; //La velocidad a la que se mueve el personaje
-    public float jumpForce = 10f;
-    public bool isGrounded;
-    
-    public float turnSmoothTime = 0.1f; //La velocidad a la que rota el mono, para que no sea tan brusco.
-    float turnSmoothVelocity;
+    private CharacterController controller;
+    private Vector2 _input;//Input del jugador.
+    private Vector3 _direction;
+    [SerializeField]private float speed = 6f; //La velocidad a la que se mueve el personaje
+    [SerializeField]private float jumpForce = 10f;
+    [SerializeField]private float turnSmoothTime = 0.05f; //La velocidad a la que rota el mono, para que no sea tan brusco.
+    private float _currentVelocity;
 
-    private void Start()
+    private void Awake()
     {
-        isGrounded = true;
-        rb = this.gameObject.GetComponent<Rigidbody>();//Se obtiene el Rigidbody del objeto/personaje.
+        controller = GetComponent<CharacterController>();
     }
-
     private void Update()
-    {   /*We get the inputs here*/
+    {
+        if (_input.sqrMagnitude == 0) return;
+        var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0, angle, 0);
+
+        controller.Move(_direction*speed * Time.deltaTime);
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
+        _direction = new Vector3(_input.x, 0.0f, _input.y);//Se usan X y Y porque es un _input de 2 Variables.
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+
+    }
+    public void OldMovement()
+    {
+        /*//We get the inputs here
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         
-        if(direction.magnitude >= 0.1f) /*Here is for the rotation.*/
+        if(direction.magnitude >= 0.1f) //Here is for the rotation.
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg+ cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            /*This is to move while taking into consideration the camera.*/
+            //This is to move while taking into consideration the camera.
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
-        /*Logica de Salto simple*/
+        //Logica de Salto simple
         if ((Input.GetButton("Jump"))&&(isGrounded==true))//Se usa la variable interna en lo que se ve como hacerle para la de Controller.
         {
             rb.AddForce(transform.up * jumpForce);
             //rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.VelocityChange);
             //isGrounded = false;
-        }
-    }
-    /*
-    private void FixedUpdate() //Movimiento usando rigidbody.
-    {
-        rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.VelocityChange);//Ignora gravedad y Drag
-    }*/
-    public void Move()
-    {
-        Debug.Log("PlayerMoves!");
-    }
-    public void Jump(InputAction.CallbackContext context)
-    {
-
+        }*/
     }
 }
