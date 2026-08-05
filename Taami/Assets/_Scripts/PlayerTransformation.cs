303ccd3b5efd0d1a0bb4af3c7ef377bb5711b9bd
Jesusxd96 <33816389+Jesusxd96@users.noreply.github.com>
2026-08-04 22:07:10 -0600

    August 4 Update
    - Create a Player prefab(Will be updated).
    - Updated transformation script to actually work.

--- Taami/Assets/_Scripts/PlayerTransformation.cs
+++ Taami/Assets/_Scripts/PlayerTransformation.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerTransformation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /*0 Forma base, 1 Zorro*/
+    //No se si sea mejor el usar gameobjects directo o una lista sencilla y que de ahi
+    #region Transformaciones
+    public List<GameObject> taamiForms = new List<GameObject>();//Lista para las transformaciones en formato GameObject 
+    private GameObject previousForm;
+    private GameObject currentForm;
+    private GameObject newForm;
+    #endregion
+
+    private void Start()//Siempre se iniciara con la forma base
     {
-        
+        //currentForm = player;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void ChangeForm(string forma)//Con este se podra cambiar de forma
+    {//Recibe un string que interpreta para hacer el cambio
+        //newForm = FindObjectOfType<GameObject>().tag(forma);
     }
 }
