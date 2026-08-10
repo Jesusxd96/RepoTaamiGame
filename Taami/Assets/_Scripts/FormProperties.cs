3e6df0a272843d3302f69128e1d0002730d0cb42
Jesusxd96 <33816389+Jesusxd96@users.noreply.github.com>
2026-08-10 13:17:56 -0600

    Implementacion cambio de forma
    - Haciendo tests de un cambio de forma basico, entre humana y zorro.
    - Ya se puede transformar.
    - Se removio el script del Character controller del humano y del fox para evitar redundancia de codigo.
    - Se les creo un script para que de este se hereden los valores unicos de cada forma.

---
+++ Taami/Assets/_Scripts/FormProperties.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormProperties : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpPower = 10f;
+    public int maxNumberOfJumps = 2;
+    public float rotationSpeed = 500f;
+    public float gravityMultiplier = 1.5f;
+}
--- Taami/Assets/_Scripts/PlayerTransformation.cs
+++ Taami/Assets/_Scripts/PlayerTransformation.cs
@@ -14,13 +14,51 @@ public class PlayerTransformation : MonoBehaviour
     private GameObject newForm;
     #endregion
 
+    public CharacterController valuesToPass;//Los valores que se le enviaran al character controller principal.
+    public CharacterController tempValues;//Valores que se guardan del Child para pasarlo al Parent
+
     private void Start()//Siempre se iniciara con la forma base
     {
-        //currentForm = player;
+        currentForm = taamiForms[0];//La 0 siempre siempre sera la forma humanoide
+        //tempValues = currentForm.GetComponentInChildren<CharacterController>();//Se consiguen los valores del hijo.
+        //valuesToPass = tempValues;
+        //this.gameObject.GetComponent<CharacterController>(CharacterController).Equals valuesToPass;//Pasarle los valores del otro al 
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ChangeForm();
+        }
     }
 
-    void ChangeForm(string forma)//Con este se podra cambiar de forma
-    {//Recibe un string que interpreta para hacer el cambio
-        //newForm = FindObjectOfType<GameObject>().tag(forma);
+    void ChangeForm()//Con este se podra cambiar de forma
+    {
+        if (currentForm==taamiForms[0])
+        {
+            taamiForms[0].SetActive(false);
+            taamiForms[1].SetActive(true);
+            currentForm = taamiForms[1];
+            CopyCharacterController(taamiForms[1].GetComponent<CharacterController>(),this.gameObject.GetComponent<CharacterController>());
+        }
+        else if(currentForm== taamiForms[1])
+        {
+            taamiForms[1].SetActive(false);
+            taamiForms[0].SetActive(true);
+            currentForm = taamiForms[0];
+            CopyCharacterController(taamiForms[0].GetComponent<CharacterController>(), this.gameObject.GetComponent<CharacterController>());
+        }
+    }
+    void CopyCharacterController(CharacterController source, CharacterController destination)//Copia los 
+    {
+        destination.radius = source.radius;
+        destination.height = source.height;
+        destination.center = source.center;
+        destination.slopeLimit = source.slopeLimit;
+        destination.stepOffset = source.stepOffset;
+        destination.skinWidth = source.skinWidth;
+        destination.minMoveDistance = source.minMoveDistance;
+        destination.detectCollisions = source.detectCollisions;
+        destination.enableOverlapRecovery = source.enableOverlapRecovery;
     }
 }
